b74be93ab6b7684c8625cd04b1d4f2cd72197c4e
burakcersit <burak.cersit@hotmail.com>
2023-09-01 11:58:09 -0400

    arrow func

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,20 +9,20 @@
 
 // Functions
 
-// function logger(){
-//     console.log('burak')
-// }
-// logger();
+function logger(){
+    console.log('burak')
+}
+logger();
 
-// function fruitProcessor(apples, oranges){
-// console.log(apples, oranges)
-// const juice = `juice with ${apples} apples and ${oranges} oranges.`;
-// return juice;
-// }
+function fruitProcessor(apples, oranges){
+console.log(apples, oranges)
+const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+return juice;
+}
 
 
-// const appleJuice = fruitProcessor(5, 6);
-// console.log(appleJuice);
+const appleJuice = fruitProcessor(5, 6);
+console.log(appleJuice);
 
 function calAge1(birthyear){
  return 2037 - birthyear   
@@ -34,9 +34,10 @@ console.log(age1)
 
 const calAge2 = function calAge1(birthyear){
     return 2037 - birthyear   
-     }
+}
+const age2 = calAge2 (1780);
+console.log(age1, age2)
 
-     const age2 = calAge2 (1780);
-
-     console.log(age1, age2)
-     
+// Arrow Function
+const calAge3 = birthyear => 2037 - birthyear
+console.log(calAge3(1500))

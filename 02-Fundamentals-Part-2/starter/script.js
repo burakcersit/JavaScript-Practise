51c04c8d6f6bd23c532adb791b833d6194af76f2
burakcersit <burak.cersit@hotmail.com>
2023-09-01 11:39:10 -0400

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 // 'use strict';
 
+
 // // let hasDriversLicence = false;
 // // let passTest = true;
 // // if(passTest) hasDriversLicence = true; 
@@ -13,12 +14,29 @@
 // }
 // logger();
 
-function fruitProcessor(apples, oranges){
-console.log(apples, oranges)
-const juice = `juice with ${apples} apples and ${oranges} oranges.`;
-return juice;
+// function fruitProcessor(apples, oranges){
+// console.log(apples, oranges)
+// const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+// return juice;
+// }
+
+
+// const appleJuice = fruitProcessor(5, 6);
+// console.log(appleJuice);
+
+function calAge1(birthyear){
+ return 2037 - birthyear   
+
 }
 
+const age1 = calAge1(1050)
+console.log(age1)
 
-const appleJuice = fruitProcessor(5, 6);
-console.log(appleJuice);
+const calAge2 = function calAge1(birthyear){
+    return 2037 - birthyear   
+     }
+
+     const age2 = calAge2 (1780);
+
+     console.log(age1, age2)
+     
